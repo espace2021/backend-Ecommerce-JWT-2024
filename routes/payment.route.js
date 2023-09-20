89736d29f0b3dd8451f56f1501e6b0dbb1a70942
espace2021 <92257192+espace2021@users.noreply.github.com>
2023-09-20 16:51:37 +0200

    Update payment.route.js

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const stripe = require('stripe');
-const Stripe = stripe('sk_test_51KtYRUD3HS4vNAwa7ANL32HQqRTywhV3JHWIp3BxAIHv04bWoz22aKlRs9Q1L6znSX2i5fu5i3Xkl9i2Goz7jAkC00LL0T3lTL');
+const stripe = require('stripe')('sk_test_51KtYRUD3HS4vNAwa7ANL32HQqRTywhV3JHWIp3BxAIHv04bWoz22aKlRs9Q1L6znSX2i5fu5i3Xkl9i2Goz7jAkC00LL0T3lTL');
 
 router.post('/', async (req, res) => { console.log(req.body)
     let status, error;
